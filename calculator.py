d8b19c8b71d9bc29f02489639b0fd85c1e26b811
HoliHoliHoli <holisterogah@gmail.com>
2020-03-26 19:30:22 +0100

    formula correction

--- calculator.py
+++ calculator.py
@@ -3,7 +3,7 @@ from math import pi
 """This program finds the area of a circle when given it's radius"""
 
 radius = int(input("What is the radius of your circle?"))
-area = radius * pi
+area = pi * radius * radius
 area = round(area, 2)  # area to 2 decimal places
 
 print("The area of your circle is ", area)
