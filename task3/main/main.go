b9a1414209395af396b2f29883302702b2c9258a
HoliHoliHoli <holisterogah@gmail.com>
2020-04-23 23:11:09 +0100

    figuring out json 2

--- task3/main/main.go
+++ task3/main/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"holi/task3/websites"
 	"holi/task3/socialmedia"
-	//"encoding/json"
+	"encoding/json"
 	//"encoding/xml"
 	"os"
 	"errors"
@@ -14,16 +14,16 @@ func main() {
 	fb := new(websites.Facebook)
 	twtr := new(websites.Twitter)
 	lnkdin := new(websites.Linkedin)
-	err := export(twtr, "twtrdata.json")
-	err = export(fb, "fbdata.txt")
-	err = export(lnkdin, "lnkdin.txt")
+	err := export(twtr, []string{"twtrdata.txt", "twtrdata.json"})
+	err = export(fb, []string{"fbdata.txt", "fbdata.json"})
+	err = export(lnkdin, []string{"lnkdin.txt"})
 
 	if err != nil {
 		panic(err)
 	}
 
 
-	ScrollFeeds(fb, twtr, lnkdin)
+	//ScrollFeeds(fb, twtr, lnkdin)
 }
 
 // ScrollFeeds prints all social media feeds
@@ -36,36 +36,38 @@ func ScrollFeeds(platforms ...socialmedia.SocialMedia) {
 	}
 }
 
-func export(u socialmedia.SocialMedia, filename string) error {
-	f, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, 0755)
-	if err != nil {
-		return errors.New("an error occured opening the file: " + err.Error())
-	}
-
-	switch filename[len(filename)-3:] {
-	case "txt":
-		for _, fd := range u.Feed() {
-			n, err := f.Write([]byte(fd + "\n"))
-			if err != nil {
-				return errors.New("an error occured writing to file: " + err.Error())
-			}
-			fmt.Printf("wrote %d bytes\n", n)
+func export(u socialmedia.SocialMedia, filename []string) error {
+	for _, ban := range (filename){
+		f, err := os.OpenFile(ban, os.O_CREATE|os.O_WRONLY, 0755)
+		if err != nil {
+			return errors.New("an error occured opening the file: " + err.Error())
 		}
-/*
-	case "son":
-		//u, _ := json.Marshal(u)
-		for abc, fd := range u.Feed() {
-			very := map[int]string{abc:fd}
-			n, err := f.Write([]byte(very + "\n"))
-			if err != nil {
-				return errors.New("an error occured writing to file: " + err.Error())
-			}
-			fmt.Printf("wrote %d bytes\n", n)
-		}
-	case "xml" */
-	}
 
-	
+		switch ban[len(ban)-3:] {
+		case "txt":
+			for _, fd := range u.Feed() {
+				n, err := f.Write([]byte(fd + "\n"))
+				if err != nil {
+					return errors.New("an error occured writing to file: " + err.Error())
+				}
+				fmt.Printf("wrote %d bytes\n", n)
+			}
+
+		case "son":
+			//u, _ := json.Marshal(u)
+			for abc, fd := range u.Feed() {
+				very := map[int]string{abc:fd}
+				so, _ := json.Marshal(very)
+				n, err := f.Write([]byte(string(so) + "\n"))
+				if err != nil {
+					return errors.New("an error occured writing to file: " + err.Error())
+				}
+				fmt.Printf("wrote %d bytes\n", n)
+			}
+		//case "xml" 
+		}
+
+	}
 	return nil
 }
 
